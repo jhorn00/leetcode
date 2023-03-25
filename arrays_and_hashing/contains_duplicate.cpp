03edee2f326ec566b16e1dba1489668fcb7976e3
James Horn <dawshorn@gmail.com>
2023-03-24 20:59:05 -0400

    initial

---
+++ arrays_and_hashing/contains_duplicate.cpp
@@ -0,0 +1,13 @@
+// These will probably end up with a ton of includes for the sake of saving time
+// I am not going to be organizing the includes
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+    }
+};
