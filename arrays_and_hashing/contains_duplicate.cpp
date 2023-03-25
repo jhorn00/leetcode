abdb397c96679aa2afb441169af16075eb7641d7
jhorn00 <dawshorn@gmail.com>
2023-03-24 21:47:49 -0400

    contains duplicate easy

--- arrays_and_hashing/contains_duplicate.cpp
+++ arrays_and_hashing/contains_duplicate.cpp
@@ -4,10 +4,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
+#include <set>
+#include <unordered_map>
 
+// for leetcode :(
+using namespace std;
+
+// This is Time: O(n) and Space O(n)
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        
+        unordered_set<int> haveIt;
+        for (int i : nums)
+        {
+            if(haveIt.find(i) != haveIt.end())
+            {
+                return true;
+            }
+            haveIt.emplace(i);
+        }
+        return false;
     }
 };
