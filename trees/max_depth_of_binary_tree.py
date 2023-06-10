c0658026db94af1c8b103260cbea0ac7d4113bf6
jhorn00 <dawshorn@gmail.com>
2023-06-09 20:01:56 -0400

    max depth of binary tree

---
+++ trees/max_depth_of_binary_tree.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+# Time: O(n), must iterate over each node
+# Space: O(n), stores 3 depth values for each node visited (in addition to recursive overhead)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # if None, adds 0 to depth
+        if not root:
+            return 0
+        # depth current node adds
+        depth = 1
+
+        # get each branch depth
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        # add the highest depth of the two branches
+        depth += max(left, right)
+        
+        # return depth of entire (sub)tree from current root
+        return depth
